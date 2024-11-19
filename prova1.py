a9ecb81094cb0f6ad9cc45221eb2a08c4f02942e
flavialcr <flavia.lcr@hotmail.com>
2024-11-19 20:51:57 -0300

    Create prova1.py

---
+++ prova1.py
@@ -0,0 +1,17 @@
+#Crie um exemplo de cada uma das principais tipagens de variáveis.
+#Crie uma solicitação de dados ao usuário.
+#Imprima a solicitação anterior na tela com uma mensagem personalizada.
+
+#string -> conjunto de caracteres ordenados 
+nome = 'nome'
+#int -> intenger - números inteiros
+idade = 32
+#float -> números decimais (não inteiros)
+salario = 5600.72
+#bool -> True ou Falso (verdadeiro ou falso)
+feliz = True
+#pedir um dado -> input()
+nome = input('Digite o seu nome por favor: ')
+
+#mostrar na tela com mensagem personalizada -> print (f-string)
+print(f'Seja muito bem vindo(a), {nome}')
